9b1557dc5f63fb53e485d75c995ee9ab93136052
Bogdan Artyushenko <Bogdan.Artyushenko@gmail.com>
2023-10-29 18:48:21 +0200

    init mvn

---
+++ src/main/java/edu/naukma/kb/rules/rulesconflict/RulesConflictApplication.java
@@ -0,0 +1,13 @@
+package edu.naukma.kb.rules.rulesconflict;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RulesConflictApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RulesConflictApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/naukma/kb/rules/rulesconflict/RulesConflictApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.naukma.kb.rules.rulesconflict;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RulesConflictApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
